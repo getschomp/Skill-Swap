2644c12d9464d79bf6c4f4e546474ec26dadbf81
Allison <allisonmariebrowne@gmail.com>
2015-01-18 10:16:46 -0500

    working through sorting problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,36 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :wanted_skills, :update_only => true
   accepts_nested_attributes_for :had_skills, :update_only => true
 
-  def sort_by_intersection(had_skill, wanted_skill)
+  # def sort_by_intersection(had_skill_id, wanted_skill_id)
+  #   matching_had_skill = []
+  #   had_skills = HadSkill.all
+  #   had_skills.each do |had_skill|
+  #     if had_skill.id == had_skill_user_id
+  #       matching_had_skill << HadSkill.find_by(user_id: had_skill.user_id)
+  #     end
+  #   end
+  #
+  # end
+  #
+  # def sort_by_intersection(had_skill_id, wanted_skill_id)
+  #   match = []
+  #   users = User.all
+  #   users.each do |user|
+  #     user.had_skills.each do |had_skill|
+  #       user.wanted_skills.each do |wanted_skill|
+  #         if had_skill.id == had_skill_id && wanted_skill.id == wanted_skill_id
+  #           match << user
+  #         end
+  #       end
+  #     end
+  #   end
 
+  def sort_by_intersection(had_skill_id, wanted_skill_id)
+    match = []
+
+    # find all users where had_skill_id matches and wanted_skill_id
+    # and wanted_skill_id mathes had skill id
+      HadSkill.where(skill_id: had_skill.id)
   end
 
   def sort_alphabetically
